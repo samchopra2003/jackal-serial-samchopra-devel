03c5310bce2f7a1c5cb3f790b09e74c7796c3324
Samarth Chopra <samarthchoprapws@gmail.com>
2026-04-03 12:20:41 -0400

    updated for frsky rc

--- jackal-teleop/src/jackal_teleop.cpp
+++ jackal-teleop/src/jackal_teleop.cpp
@@ -28,8 +28,6 @@ public:
     twist_pub_ = this->create_publisher<geometry_msgs::msg::TwistStamped>(
       "/jackal_velocity_controller/cmd_vel", 10);
     is_auto_pub_ = this->create_publisher<std_msgs::msg::Bool>("is_auto", 10);
-    trigger_pub_ = this->create_publisher<std_msgs::msg::UInt8>("trigger", 10);
-    is_streaming_pub_ = this->create_publisher<std_msgs::msg::Bool>("is_streaming", 10);
 
     pub_timer_ = this->create_wall_timer(
       std::chrono::milliseconds(20),
@@ -54,32 +52,11 @@ private:
     twist_buf_.twist.linear.x = apply_deadzone(msg->axes[3]) * 2.0;
     twist_buf_.twist.angular.z = 1.5 * apply_deadzone(msg->axes[2]) * 0.5;
 
-    is_auto_ = msg->buttons[1];
-    trigger_ = msg->buttons[0];
-    is_streaming_ = msg->buttons[2];
+    is_auto_ = msg->axes[4] > 0;
 
     std_msgs::msg::Bool is_auto_msg;
     is_auto_msg.data = is_auto_;
     is_auto_pub_->publish(is_auto_msg);
-
-    std_msgs::msg::Bool is_streaming_msg;
-    is_streaming_msg.data = is_streaming_;
-    is_streaming_pub_->publish(is_streaming_msg);
-
-    if (is_auto_) {
-      trigger_ = 0;
-      return;
-    }
-
-    if (trigger_ == 1 && prev_trigger_ == 0) {
-      std_msgs::msg::UInt8 trigger_msg;
-      trigger_msg.data = trigger_count_;
-      trigger_pub_->publish(trigger_msg);
-
-      trigger_count_ = (trigger_count_ + 1) % num_views_;
-    }
-
-    prev_trigger_ = trigger_;
   }
 
   geometry_msgs::msg::TwistStamped twist_buf_;
@@ -93,8 +70,6 @@ private:
   rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr joy_sub_;
   rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twist_pub_;
   rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr is_auto_pub_;
-  rclcpp::Publisher<std_msgs::msg::UInt8>::SharedPtr trigger_pub_;
-  rclcpp::Publisher<std_msgs::msg::Bool>::SharedPtr is_streaming_pub_;
   rclcpp::TimerBase::SharedPtr pub_timer_;
 };
 
